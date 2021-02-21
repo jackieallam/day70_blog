6591ad25f9097e5e886f7c3202f02edbf4e758c1
Jackie Allam <jackieannallam@gmail.com>
2021-02-22 01:18:19 +0200

    Fix comment submit button

--- forms.py
+++ forms.py
@@ -29,4 +29,4 @@ class LoginForm(FlaskForm):
 
 class CommentForm(FlaskForm):
     comment = CKEditorField("Comment", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+    submit = SubmitField("Submit Comment")
